1808d4db6d0dd674cdc78d8fbc2e8b049afd4f3c
subjav <regdomainkris@gmail.com>
2026-03-17 22:02:40 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+var CACHE = 'subjav-v1';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(['./']);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
